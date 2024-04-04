48cd9575c06cfd5302b94518a0a6ffc97ad48ce4
Vsanotskyi <vsanotskyi@gmail.com>
2024-04-04 11:49:27 +0300

    add api, redux for month stats, add constants for test

--- src/API/ApiMonthStats.js
+++ src/API/ApiMonthStats.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { urls } from '../constants/urls.js';
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MGM1YWZhOWY2MjU2ZjE2MjY0N2IzOSIsImlhdCI6MTcxMjIxODI5MiwiZXhwIjoxNzE0ODEwMjkyfQ.gMdNhWpvfJM-AqFLc6BT7K68-urxliU-j7Q6X_XORy4';
+const token = '';
 
 const config = {
   baseURL: urls.baseURL,
