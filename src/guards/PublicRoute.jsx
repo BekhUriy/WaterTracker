f782d7f91f54dbe63b5fb54e6a4d5e9691a80131
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-04-04 17:44:36 +0300

    Update PublicRoute.jsx

--- src/guards/PublicRoute.jsx
+++ src/guards/PublicRoute.jsx
@@ -3,7 +3,7 @@ import { isAuthSelector } from '../redux/auth/selectors';
 import { Navigate, useLocation } from 'react-router-dom';
 import { Component } from 'react';
 
-const PublicRoute = ({ component: Component, redirectTo = '/' } ) => {
+const PublicRoute = ({ component: Component, redirectTo } ) => {
   const isAuth = useSelector(isAuthSelector);
   // const { state: prevLocation } = useLocation();
   return isAuth ? <Navigate to={redirectTo}/> : Component  ;
