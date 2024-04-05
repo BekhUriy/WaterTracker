734d8c6979ec4f2f9206c14b7893d5a6a1553106
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-04-05 13:16:28 +0300

    Update selectors.js

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,5 +1,6 @@
 export const selectToken = state => state.auth.token;
 export const isLoggedInSelector = (state) => state.auth.isLoggedIn;
+export const profileSelector = (state) => state.auth.profile;
 export const selectUser = state => state.auth.user;
 export const selectMessage = state => state.auth.message;
 export const selectIsLogin = state => state.auth.message;
