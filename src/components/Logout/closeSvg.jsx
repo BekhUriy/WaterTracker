b9419b81074c5b4c7cb3d4deacad8e067dcedc44
KovalUser19 <134967853+KovalUser19@users.noreply.github.com>
2024-04-05 17:40:38 +0300

    add CloseModal

--- src/components/Logout/closeSvg.jsx
+++ src/components/Logout/closeSvg.jsx
@@ -12,6 +12,7 @@ export const CloseSvg = ({ color}) => {
       strokeWidth="3"
       strokeLinecap="round"
       strokeLinejoin="round"
+
     >
       <path d="M18 6L6 18M6 6l12 12" />
     </svg>
