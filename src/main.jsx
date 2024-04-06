3017cbe57342e91851a854e713a29e84d686ba7f
KovalUser19 <134967853+KovalUser19@users.noreply.github.com>
2024-04-06 21:12:21 +0300

    add

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,8 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
-/* import App from './components/App'; */
+import App from './components/App';
 import ModalProvider from './components/Home/DailyNorma/DailyNormaModal/ModalProvider/ModalProvider';
-
-import { UserLogoutModal } from './components/Logout/UserLogoutModal';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,8 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <ModalProvider>
-            <UserLogoutModal />
-            {/* <App /> */}
+            <App />
           </ModalProvider>
         </PersistGate>
       </Provider>
