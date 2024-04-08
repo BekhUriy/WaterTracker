87d047770a1d7c400e66f8e6bd70e36a960b2894
Vsanotskyi <vsanotskyi@gmail.com>
2024-04-09 00:57:32 +0300

    com

--- src/API/ApiUser.js
+++ src/API/ApiUser.js
@@ -1,11 +1,16 @@
 import { apiServices } from './apiServices';
 
-export const updateApi = async (updates) => {
+export const updateNameGenderApi = async (updates) => {
   const { data } = await apiServices.patch('user/update', updates);
   return data;
 };
 
 export const updateAvatarApi = async (avatarURL) => {
   const { data } = await apiServices.patch('user/avatar', avatarURL);
+  return data;
+};
+
+export const updatePassApi = async (pass) => {
+  const { data } = await apiServices.patch('auth/updatepassword', pass);
   return data;
 };
