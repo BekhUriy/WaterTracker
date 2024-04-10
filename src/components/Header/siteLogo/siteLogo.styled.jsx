df239373a0f62185a1bc218ae03eff0df70fa8cf
LucreciaUA <118127063+LucreciaUA@users.noreply.github.com>
2024-04-11 00:18:48 +0300

    1

--- src/components/Header/siteLogo/siteLogo.styled.jsx
+++ src/components/Header/siteLogo/siteLogo.styled.jsx
@@ -4,4 +4,8 @@ import styled from "styled-components";
 
 export const LogoImg = styled.img`
     cursor: pointer;
+         &:hover {
+        transform: scale(1.1);
+        transition: transform 450ms ease-in-out;
+    }
 `
