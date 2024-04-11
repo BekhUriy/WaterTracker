6d1612ee3435e4df6d54cc9fa93fd7ca4007ffbd
BekhUriy <131916270+BekhUriy@users.noreply.github.com>
2024-04-11 10:36:32 +0000

    Deploying to gh-pages from @ BekhUriy/WaterTracker@fc19ae974cd0d9937176be3f81c2e620141253f4 🚀

--- assets/ErrorPage-06823ca7.js
+++ assets/ErrorPage-06823ca7.js
@@ -1,4 +1,4 @@
-import{u as t,j as e}from"./index-0db364db.js";import{C as n,B as i}from"./Signup.styled-7792d6bb.js";const o=t.h1`
+import{u as t,j as e}from"./index-c41d2607.js";import{C as n,B as i}from"./Signup.styled-ed7ddc4d.js";const o=t.h1`
 margin-top: 15px;
 font-size: 32px;
 font-weight: 400;
